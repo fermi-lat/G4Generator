83e42ae02e608a7db50eef3aeef5ba5cc7c77671
riccardo <>
2001-11-26 18:00:20 +0000

    First draft versions of DetectorConstruction, G4SectionsVisitor, PhysicsList, PrimaryGeneratorAction classes

--- src/G4Generator.h
+++ src/G4Generator.h
@@ -1,4 +1,4 @@
-// $Header:$
+// $Header: /nfs/slac/g/glast/ground/cvs/G4Generator/src/G4Generator.h,v 1.1.1.1 2001/11/19 02:55:42 burnett Exp $
 
 
 #ifndef G4Generator_h
@@ -9,6 +9,8 @@
 #include <string>
 class IFluxSvc;
 class IFlux;
+class G4RunManager;
+class PrimaryGeneratorAction;
 
 /**
   Geant4 interface
@@ -26,6 +28,11 @@ private:
     IFlux*    m_flux;
 
     std::string m_source_name;
+
+    /// This is the G4 manager that handles the simulation
+    G4RunManager* m_runManager;
+    /// This is the primaries generator class 
+    PrimaryGeneratorAction* m_primaryGenerator;
 };
 
 #endif
