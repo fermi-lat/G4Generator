686558141803cf1b419f738de8d0824541a8a41c
burnett <>
2001-12-16 16:15:03 +0000

    add display options

--- src/G4Generator.h
+++ src/G4Generator.h
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/G4Generator/src/G4Generator.h,v 1.3 2001/11/30 17:11:56 riccardo Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/G4Generator/src/G4Generator.h,v 1.4 2001/12/02 22:23:24 burnett Exp $
 
 
 #ifndef G4Generator_h
@@ -11,7 +11,7 @@
 class IFluxSvc;
 class IFlux;
 class RunManager;
-
+namespace gui{class GuiMgr;}
 /**
   Geant4 interface for GLAST simulation
   */
@@ -23,6 +23,8 @@ public:
     StatusCode execute();
     StatusCode finalize();
     
+    void setSource(std::string source_name);
+
 private:
     IFluxSvc* m_fluxSvc;
     IFlux*    m_flux;
@@ -33,7 +35,6 @@ private:
     /// set of UI commands for setup
     StringArrayProperty m_UIcommands;
 
-//    std::vector<std::string> m_UIcommands;
 
     // the top volume for instantiating the detector
     std::string m_topvol;
@@ -43,6 +44,10 @@ private:
 
     /// This is the G4 manager that handles the simulation
     RunManager* m_runManager;
+
+    /// access to the GuiManager
+    gui::GuiMgr* m_guiMgr;
+    void setupGui();
 };
 
 #endif
