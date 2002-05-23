f00081985e4c71905c9113c89f415132fc4f55ef
riccardo <>
2002-05-23 09:55:16 +0000

    Added a sign reversal for rotation in G4Geometry to be consistent with rotations conventions. Fixed an error in the McParticle tree building for which there were two primaries in the tree.

--- src/G4Geometry.cxx
+++ src/G4Geometry.cxx
@@ -1,5 +1,5 @@
 // File and Version Information:
-// $Header: /nfs/slac/g/glast/ground/cvs/G4Generator/src/G4Geometry.cxx,v 1.12 2002/04/19 07:44:41 riccardo Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/G4Generator/src/G4Geometry.cxx,v 1.13 2002/04/19 12:51:30 riccardo Exp $
 //
 // Description: this is a concrete implementation of the abstract interface
 // IGeometry of GlastDetSvc. Its methods are called by the visitor mechanism of
@@ -128,11 +128,13 @@ G4Geometry::pushShape(ShapeType s, const UintVector& idvec,
       m_replica = 1;
     }
 
-  // Set the rotation: note conversion to radians
+  // Set the rotation: note conversion to radians and change of sign
+  // The sign is changed for different conventions between our XML
+  // geometry database and G4 ones.
   G4RotationMatrix* rm = new G4RotationMatrix(); 
-  rm->rotateX(rx*M_PI/180);
-  rm->rotateY(ry*M_PI/180);
-  rm->rotateZ(rz*M_PI/180);
+  rm->rotateX(-rx*M_PI/180);
+  rm->rotateY(-ry*M_PI/180);
+  rm->rotateZ(-rz*M_PI/180);
 
   // Create the positioned physical volume
   phys = new G4PVPlacement(rm,G4ThreeVector(x*mm,y*mm,z*mm),
