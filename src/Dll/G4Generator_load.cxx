7dcd4730d9581ee6f0ae18849c1b6f3e9db8d5ca
usher <>
2003-01-23 16:15:05 +0000

    Removed files associated with Propagator Created a G4GeometrySvc to handle the Geant4 (and propagator) geometry Modified G4Generator and RunManager accordingly

--- src/Dll/G4Generator_load.cxx
+++ src/Dll/G4Generator_load.cxx
@@ -3,13 +3,13 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/G4Generator/src/Dll/G4Generator_load.cxx,v 1.6 2002/04/11 23:20:21 usher Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/G4Generator/src/Dll/G4Generator_load.cxx,v 1.7 2002/10/08 22:15:15 kyoung Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 DECLARE_FACTORY_ENTRIES(G4Generator) {
     DECLARE_ALGORITHM( G4Generator);
-    DECLARE_SERVICE(   G4PropagatorSvc);
+    DECLARE_SERVICE(   G4GeometrySvc);
     DECLARE_TOOL(   G4GeneratorRandom);
 } 
