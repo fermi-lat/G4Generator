c3c0cbf410d358e258c48ca8c14d88ee88beb443
burnett <>
2005-12-07 20:03:46 +0000

    put in preliminary version of the beam transform guy.

--- src/Dll/G4Generator_load.cxx
+++ src/Dll/G4Generator_load.cxx
@@ -3,7 +3,7 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/G4Generator/src/Dll/G4Generator_load.cxx,v 1.8 2003/01/23 16:15:05 usher Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/G4Generator/src/Dll/G4Generator_load.cxx,v 1.9 2005/09/21 03:56:22 usher Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
@@ -14,4 +14,5 @@ DECLARE_FACTORY_ENTRIES(G4Generator)
     DECLARE_SERVICE(   G4GeometrySvc);
     DECLARE_SERVICE(   G4GenErrorSvc);
     DECLARE_TOOL(      G4GeneratorRandom);
+    DECLARE_ALGORITHM( BeamTransform);
 } 
