8214e940f0d9bf56f2dcc7871e9d33ce9192781b
usher <>
2007-02-15 20:51:11 +0000

    fix linux compilation?

--- src/G4Generator.cxx
+++ src/G4Generator.cxx
@@ -1,7 +1,7 @@
 /** @file G4Generator.cxx
     @brief implementation of class G4Generator
 
-    $Header: /nfs/slac/g/glast/ground/cvs/G4Generator/src/G4Generator.cxx,v 1.63 2007/02/01 17:38:44 usher Exp $
+    $Header: /nfs/slac/g/glast/ground/cvs/G4Generator/src/G4Generator.cxx,v 1.64 2007/02/15 19:13:53 usher Exp $
 
  This is the Gaudi algorithm that runs Geant4 and fills the TDS
  with Montecarlo data. It initalizes some services (for tds and detector
@@ -216,20 +216,20 @@ StatusCode G4Generator::initialize()
   // set the mode for the McParticle tree
   if (m_mcTreeMode == "nGenerations")
   {
-      McParticleManager::getPointer()->setMode(McParticleManager::PruneMode::N_GENERATIONS);
+      McParticleManager::getPointer()->setMode(McParticleManager::N_GENERATIONS);
       McParticleManager::getPointer()->setNumGenerations(m_numGenerations);
   }
   else if (m_mcTreeMode == "prunecal")
   {
-      McParticleManager::getPointer()->setMode(McParticleManager::PruneMode::PRUNE_CAL);
+      McParticleManager::getPointer()->setMode(McParticleManager::PRUNE_CAL);
   }
   else if (m_mcTreeMode == "full")
   {
-      McParticleManager::getPointer()->setMode(McParticleManager::PruneMode::FULL_MC_TREE);
+      McParticleManager::getPointer()->setMode(McParticleManager::FULL_MC_TREE);
   }
   else // default mode is "minimal" 
   {
-      McParticleManager::getPointer()->setMode(McParticleManager::PruneMode::MINIMAL_TREE);
+      McParticleManager::getPointer()->setMode(McParticleManager::MINIMAL_TREE);
       McParticleManager::getPointer()->setCutOffEnergy(m_lowEnergy);
   }
  
