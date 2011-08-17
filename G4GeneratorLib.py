cdd352085f2915e43fe458709131d5541e6f5a47
jrb <>
2011-08-17 00:11:53 +0000

    Get rid of unnecessary library dependencies

--- G4GeneratorLib.py
+++ G4GeneratorLib.py
@@ -1,14 +1,12 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/G4Generator/G4GeneratorLib.py,v 1.1 2008/08/15 21:42:34 ecephas Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/G4Generator/G4GeneratorLib.py,v 1.3 2009/01/23 00:20:55 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['G4Generator'])
     env.Tool('fluxLib')
-    env.Tool('G4HadronSimLib')
     env.Tool('EventLib')
     env.Tool('addLibrary', library = env['geant4Libs'])
     env.Tool('GlastSvcLib')
     env.Tool('geometryLib')
-    env.Tool('GlastMSLib')
     env.Tool('addLibrary', library = env['clhepLibs'])
 def exists(env):
     return 1;
