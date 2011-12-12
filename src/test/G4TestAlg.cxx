037ea993c4a5b3d5f4412e52be91da6e4111931c
heather <>
2011-12-12 20:49:23 +0000

    merge Gaudi v21r7

--- src/test/G4TestAlg.cxx
+++ src/test/G4TestAlg.cxx
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/G4Generator/src/test/G4TestAlg.cxx,v 1.1.1.1 2001/11/19 02:55:42 burnett Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/G4Generator/src/test/G4TestAlg.cxx,v 1.2.658.1 2010/10/08 16:27:51 heather Exp $
 
 // Include files
 
@@ -28,8 +28,9 @@ private:
 };
 
 
-static const AlgFactory<G4TestAlg>  Factory;
-const IAlgFactory& G4TestAlgFactory = Factory;
+//static const AlgFactory<G4TestAlg>  Factory;
+//const IAlgFactory& G4TestAlgFactory = Factory;
+DECLARE_ALGORITHM_FACTORY(G4TestAlg);
 
 //
 G4TestAlg::G4TestAlg(const std::string& name, ISvcLocator* pSvcLocator) :
